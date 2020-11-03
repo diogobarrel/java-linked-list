4b536e77fb70cddb5cfde5c959ae8689df8d2505
diogobarrel <diogo.barrel@stilingue.com.br>
2020-11-02 22:22:13 -0300

    chore: updated main for tests

---
+++ main.java
@@ -0,0 +1,28 @@
+import aed.collections.StackList;
+
+public class main {
+    public static void main(String[] args) {
+        StackList obj = new StackList(); 
+        // insert Stack value 
+        obj.push(11); 
+        obj.push(22); 
+        obj.push(33); 
+        obj.push(44); 
+  
+        // print Stack elements 
+        obj.display(); 
+  
+        // print Top element of Stack 
+        System.out.printf("\nTop element is %d\n", obj.peek()); 
+  
+        // Delete top element of Stack 
+        obj.pop(); 
+        obj.pop(); 
+  
+        // print Stack elements 
+        obj.display(); 
+  
+        // print Top element of Stack 
+        System.out.printf("\nTop element is %d\n", obj.peek()); 
+    }
+}
