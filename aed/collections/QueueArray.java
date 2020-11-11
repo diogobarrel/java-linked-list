9f935706d19e42dc598d33d4a471649d1acfb9e7
diogobarrel <diogo.barrel@stilingue.com.br>
2020-11-10 22:57:59 -0300

    chore: minor formatting

--- aed/collections/QueueArray.java
+++ aed/collections/QueueArray.java
@@ -1,92 +1,86 @@
 package aed.collections;
+
 import java.util.Iterator;
-import java.util.Arrays;
 import java.lang.OutOfMemoryError;
 
-public class QueueArray<T> implements Iterable<T>{
-	
+public class QueueArray<T> implements Iterable<T> {
+
 	private T queueArray[];
 	public int size, head, rear;
 	private int maxSize;
-	
+
 	@SuppressWarnings("unchecked")
 	public QueueArray(int max) {
-		maxSize = max+1;
+		maxSize = max + 1;
 		head = 1;
 		rear = 0;
 		size = 0;
-		queueArray = (T[]) new Object[maxSize];			
-	}	
+		queueArray = (T[]) new Object[maxSize];
+	}
+
 	public boolean isEmpty() {
 		return size == 0;
-		
+
 	}
-	
+
 	public int size() {
 		return size;
 	}
-	
+
 	public T peek() {
-		if(isEmpty()) {
+		if (isEmpty()) {
 			return null;
 		}
 		return queueArray[rear];
 
 	}
+
 	public void enqueue(T item) throws OutOfMemoryError {
-		if(((rear + 2) % maxSize) == head) {
+		if (((rear + 2) % maxSize) == head) {
 			throw new OutOfMemoryError();
 		}
-		
+
 		rear = (rear + 1) % maxSize;
 		queueArray[rear] = item;
 		size++;
-	}	
-		
-	
-	
+	}
+
 	public T dequeue() {
-		if(isEmpty()) {
+		if (isEmpty()) {
 			return null;
-					}
-		/*Condicional que seja sempre verdade, enquanto size > 0, visando 
-		 *somente retornar rear, sem acabar o resto do método.
+		}
+		/*
+		 * Condicional que seja sempre verdade, enquanto size > 0, visando somente
+		 * retornar rear, sem acabar o resto do método.
 		 */
 		T dataHead = queueArray[head];
 		head = (head + 1) % maxSize;
-		return dataHead;	
-		}
-		
-		
+		return dataHead;
 	}
-	
+
 	@Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-        	
-      
-            @Override
-            public boolean hasNext() {
-            	return (((rear + 2) % maxSize) == head);
-            }
+	public Iterator<T> iterator() {
+		return new Iterator<T>() {
 
-            @Override
-            public T next() {
-            	if(!hasNext()) {
-            		throw new Error();
-            	}
-           		
-            	T data = queueArray[head+1];
-           		head = head + 1;
-           		return data;
-            	};            	
-             };
-        };    
+			@Override
+			public boolean hasNext() {
+				return (((rear + 2) % maxSize) == head);
+			}
 
-	
+			@Override
+			public T next() {
+				if (!hasNext()) {
+					throw new Error();
+				}
+
+				T data = queueArray[head + 1];
+				head = head + 1;
+				return data;
+			};
+		};
+	};
+
 	public QueueArray<T> shallowCopy() {
 		return null;
 	}
-	
 }
-
